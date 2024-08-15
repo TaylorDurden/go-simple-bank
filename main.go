2b9b44052b050af5c90f1962a1fe33fb3fcba29b
Taylor Li <258266864@qq.com>
2024-08-15 21:16:05 +0800

    GBP-13 run gprc server api

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	_ "github.com/lib/pq"
 	"github.com/taylordurden/go-simple-bank/api"
 	db "github.com/taylordurden/go-simple-bank/db/sqlc"
+	"github.com/taylordurden/go-simple-bank/gapi"
+	pb "github.com/taylordurden/go-simple-bank/pb"
 	"github.com/taylordurden/go-simple-bank/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -21,6 +26,32 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("run grpc server err:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("create listener error")
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("start gRPC server error")
+	}
+}
+
+func runGinServer(config util.Config, store *db.SQLStore) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server", err)
